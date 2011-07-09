73ad0e20e57e65c1934b3b1375f3cdabd509b739
Alan Malloy <alan@malloys.org>
2011-07-08 23:14:07 -0700

    First commit

---
+++ MyBot.clj
@@ -0,0 +1,12 @@
+(ns MyBot
+  (:use ants))
+
+(def directions [:north :east :west :south])
+
+(defn simple-bot []
+  (doseq [ant (my-ants)]
+    (let [dir (rand-nth (filter #(unoccupied? (can-move? ant %)) directions))]
+      (when dir
+        (move ant dir)))))
+
+(start-game simple-bot)
